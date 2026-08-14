37de4739ade208b4c771262964eda977095c5244
Rennie Beekharry <rbeekharry@hotmail.com>
2026-08-14 16:01:43 -0400

    Use JSON-aware DoH resolver for WinRE downloads

--- tools/rescuemeai-nslookup.cmd
+++ tools/rescuemeai-nslookup.cmd
@@ -1,11 +1,13 @@
 @echo off
 setlocal EnableExtensions
-rem WR-MODULE: rescuemeai-nslookup 2026.08.14-1600-ET
+rem WR-MODULE: rescuemeai-nslookup 2026.08.14-1609-ET
 set "HOST=%~1"
-set "IP="
-call "C:\WinRERepair\runtime\resolve.cmd" resolve "%HOST%" IP >nul 2>&1
-if errorlevel 1 exit /b 1
-if not defined IP exit /b 1
-echo Name: %HOST%
-echo Address: %IP%
-exit /b 0
+set "CURL=C:\Windows\System32\curl.exe"
+set "CSCRIPT=X:\Windows\System32\cscript.exe"
+if not exist "%CSCRIPT%" set "CSCRIPT=C:\Windows\System32\cscript.exe"
+set "HELPER=C:\WinRERepair\media\doh-resolve.js"
+if not exist "%CURL%" exit /b 1
+if not exist "%CSCRIPT%" exit /b 1
+if not exist "%HELPER%" exit /b 1
+"%CSCRIPT%" //nologo "%HELPER%" "%CURL%" "%HOST%"
+exit /b %errorlevel%
