3a5a7c872978d97d54070fe8050a2e85e17171ab
Rennie Beekharry <rbeekharry@hotmail.com>
2026-09-10 01:09:00 -0400

    Make r.cmd install and launch durable reconnect shortcut

--- r.cmd
+++ r.cmd
@@ -1,4 +1,8 @@
 @echo off
-C:\Windows\System32\curl.exe --ssl-no-revoke -fL "https://raw.githubusercontent.com/RennieBeekharry/winre-repair/5902c7e130ca4712e3355f8bf489e37fca9d625f/connect-device-v3.cmd" -o X:\r3.cmd
+setlocal EnableExtensions
+if not exist C:\RescueMeAI md C:\RescueMeAI >nul 2>&1
+C:\Windows\System32\curl.exe --ssl-no-revoke -fL "https://raw.githubusercontent.com/RennieBeekharry/winre-repair/main/reconnect.cmd" -o C:\RescueMeAI\reconnect.cmd
 if errorlevel 1 exit /b 90
-call X:\r3.cmd
+> C:\r.cmd echo @echo off
+>>C:\r.cmd echo call C:\RescueMeAI\reconnect.cmd
+call C:\RescueMeAI\reconnect.cmd
