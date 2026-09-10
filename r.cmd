a105e64c94992088848d7514a934720f13d265ae
Rennie Beekharry <rbeekharry@hotmail.com>
2026-09-10 08:33:08 -0400

    Pin RescueMeAI quick reconnect to hardened reconnect v2

--- r.cmd
+++ r.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions
 if not exist C:\RescueMeAI md C:\RescueMeAI >nul 2>&1
-C:\Windows\System32\curl.exe --ssl-no-revoke -fL "https://raw.githubusercontent.com/RennieBeekharry/winre-repair/main/reconnect.cmd" -o C:\RescueMeAI\reconnect.cmd
+C:\Windows\System32\curl.exe --ssl-no-revoke -fL "https://raw.githubusercontent.com/RennieBeekharry/winre-repair/36b0dc7fa3b142845a066f7ab985952ef1dfb5db/reconnect.cmd" -o C:\RescueMeAI\reconnect.cmd
 if errorlevel 1 exit /b 90
 > C:\r.cmd echo @echo off
 >>C:\r.cmd echo call C:\RescueMeAI\reconnect.cmd
